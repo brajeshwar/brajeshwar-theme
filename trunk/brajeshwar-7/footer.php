b2a97640d1ca8040c953f01595eb22cf301086b5
unncola <unncola@b86856ca-d02c-0410-8683-ed988e09fb86>
2007-08-22 21:34:39 +0000

    updates for xhtml strict validation

--- trunk/brajeshwar-7/footer.php
+++ trunk/brajeshwar-7/footer.php
@@ -4,8 +4,8 @@
 
 <div id="footer">
 <p>
-&copy; Copyright <?php echo date('Y');?> <?php bloginfo('name'); ?> &mdash;
-<a href="http://wordpress.org/" title="Powered by WordPress <?php bloginfo('version'); ?>">WordPress</a></li>
+&copy; Copyright <?php echo date('Y');?> David Stoline &mdash;
+<a href="http://wordpress.org/" title="Powered by WordPress <?php bloginfo('version'); ?>">WordPress</a>
 Theme by <a href="http://theme.brajeshwar.com/" title="Brajeshwar Wordpress Theme">Brajeshwar</a>
 </p>
 </div>
