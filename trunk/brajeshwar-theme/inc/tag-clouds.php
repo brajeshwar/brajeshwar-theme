4e29057c71337182246c218e28ea1542b41998a7
brajeshwar <brajeshwar@b86856ca-d02c-0410-8683-ed988e09fb86>
2007-12-20 12:01:38 +0000

    Brajeshwar v8 WP Theme development moved to folder "brajeshwar-8"

--- trunk/brajeshwar-theme/inc/tag-clouds.php
+++
@@ -1,1 +0,0 @@
-<?php if ( function_exists('wp_tag_cloud') ) : ?><div id="tags-container"><h2>Site's Popular Tags</h2><?php wp_tag_cloud('smallest=8&largest=20&unit=pt&number=40&orderby=name&format=flat'); ?></div><!-- /tags-container --><span class="tags-footer"><a href="<?php bloginfo('url'); ?>/tags/" title="View all tags">View all tags</a></span><?php endif; ?>
